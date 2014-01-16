0b5c3916b9e9503d4386ee1af223699615ae40c6
blakeshall <blake@blakeshall.com>
2014-01-16 10:49:10 -0500

    Adding HTML geolocation with client side geocoding

---
+++ public/js/geolocation.js
@@ -0,0 +1,31 @@
+var address_field = document.getElementById("address");
+
+function getLocation(){
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  else{
+    // Fail quitely
+  }
+};
+function showPosition(position)
+  {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  var geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(lat, lng);
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          address_field.value = results[0].formatted_address.split(',')[0];
+        }
+      } else {
+        // Geocode Failed, fail quitely
+      }
+    });
+};
+
+jQuery(document).ready(function($) {
+  getLocation();
+});
