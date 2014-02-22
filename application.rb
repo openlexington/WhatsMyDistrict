316b23720d2269d580f8da54b9bd7d5b013f5660
Erik Schwartz <erik@erikschwartz.net>
2014-02-22 15:46:27 -0500

    Capitalize roman numeral suffix in names

--- application.rb
+++ application.rb
@@ -8,6 +8,8 @@ require 'airbrake'
 
 Dir.glob(File.join("models/**")) {|f| require "./#{f}"}
 
+ROMAN_NOOMS = ["I", "II", "III", "IV", "V"]
+
 class DistrictApp < Sinatra::Base
   set :app_file, __FILE__
   set :views, settings.root + '/views'
@@ -15,7 +17,9 @@ class DistrictApp < Sinatra::Base
   helpers do
     def word_splitter str
       return unless str
-      str.split.map {|piece| piece.capitalize}.join(' ')
+      str.split.map do |piece|
+        ROMAN_NOOMS.include?(piece) ? piece : piece.capitalize
+      end.join(' ')
     end
   end
 end
