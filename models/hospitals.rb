ca9088aaa4e32434c8a327aa5f819af483ec4650
nikolaiwarner <n@nikolaiwarner.com>
2014-02-23 09:51:47 -0500

    list closest hospitals in order

--- models/hospitals.rb
+++ models/hospitals.rb
@@ -1,14 +1,15 @@
 require_relative 'database_model'
 
 class Hospitals < DatabaseModel
-  attr_accessor :assoc_name
+  attr_accessor :name, :address
+
 
   def self.table_name
     'hospitals'
   end
 
   def self.all_for_geocode geocode
-    results = get_results("SELECT assoc_name FROM #{table_name} WHERE ST_Within(ST_SetSRID(ST_GeomFromText('#{geocode.to_point}'),4269),geom);")
+    results = get_results("SELECT name, address FROM hospitals ORDER BY geom <-> st_setsrid(ST_GeomFromText('#{geocode.to_point}'),4326);")
     results.map {|result| Hospitals.new(result)}
   rescue Sequel::DatabaseError
     []
