052f5701923b6b865f4a13a7a41f5ad59836d4e7
Mike Dillion <mike.dillion@gmail.com>
2014-02-25 19:46:37 -0500

    Change user for local deploy

--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -2,4 +2,4 @@ role :app, %w{ubuntu@localhost}
 role :web, %w{ubuntu@localhost}
 role :db,  %w{ubuntu@localhost}
 
-server 'localhost', user: 'ubuntu', roles: %w{web db app}
+server 'localhost', user: 'whatsmydistrict', roles: %w{web db app}
